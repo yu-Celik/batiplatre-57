09dd6b595971ef2bf9da16086396c6f0d18b731f
yu-Celik <celik.yus@outlook.com>
2024-05-15 00:09:17 +0200

    fdsgdsc

--- app/components/Slider/SlideHeroSection.tsx
+++ app/components/Slider/SlideHeroSection.tsx
@@ -52,8 +52,8 @@ export default function SlideHeroSection({ images, styleSlide }: { images: { ima
                             alt={image.alt}
                             loading={index === 0 ? "eager" : "lazy"}
                             priority={index === 0 ? true : false}
-                            placeholder='blur'
-                            // sizes="100vw"
+                            // placeholder='blur'
+                            sizes="100vw"
                             quality={100}
                             fill
                             style={{
