d77adb6211158778edf3f78c81a8a74016a5c09e
yu-Celik <celik.yus@outlook.com>
2024-08-17 23:31:35 +0200

    maj

--- src/app/(pages)/accueil/components/SectionSix/RenderMobile.tsx
+++ src/app/(pages)/accueil/components/SectionSix/RenderMobile.tsx
@@ -22,7 +22,7 @@ function RenderMobile() {
             </Stack>
 
             <Typography variant="body1" gutterBottom>
-                Que vous souhaitiez rénover votre maison, votre appartement ou entreprendre des travaux de rénovation dans votre rue, notre entreprise de rénovation à Metz est là pour vous accompagner. Nous sommes spécialisés dans l&apos;aménagement intérieur et extérieur, l&apos;isolation thermique, et la rénovation énergétique.
+                Que vous souhaitiez rénover votre maison, votre appartement ou entreprendre des travaux de rénovation dans votre rue, notre entreprise de rénovation à Metz est là pour vous accompagner. Nous sommes spécialisés dans l&apos;aménagement intérieur et extérieur, la rénovation intérieure, l&apos;isolation thermique, et la rénovation énergétique.
             </Typography>
             <Typography variant="body1" gutterBottom>
                 Notre équipe d&apos;artisans professionnels maîtrise tous les corps de métiers nécessaires pour transformer votre intérieur. De l&apos;aménagement des combles à la rénovation complète des pièces de vie, en passant par la pose de revêtements et la construction, nous réalisons vos projets sur mesure pour optimiser votre espace de vie.
