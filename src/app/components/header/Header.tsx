7e4eb07ffc66e344c2d0bb970a4da5fd35c84e55
yu-Celik <celik.yus@outlook.com>
2024-08-29 00:32:18 +0200

    Scroll to hide App bar

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,30 +1,53 @@
 'use client'
 
-import { AppBar, Container, Grid, Toolbar, Box, Button } from "@mui/material"
+import { AppBar, Container, Grid, Toolbar, Box, Button, useScrollTrigger } from "@mui/material"
 import Logo from "../Logo"
 import RenderMobileMenu from "@/app/components/header/mobile/RenderMobileMenu"
 import RenderDesktopMenu from "@/app/components/header/desktop/RenderDesktopMenu"
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from "@mui/material";
+import { Slide } from "@mui/material"
+import { ReactElement } from "react"
 
-function Header() {
+type Props = {
+    window?: () => Window;
+    children: ReactElement;
+}
+
+
+function HideOnScroll(props: Props) {
+    const { children, window } = props;
+    const trigger = useScrollTrigger({
+        target: window ? window() : undefined,
+    });
 
     return (
-        <AppBar role="banner" sx={{ backgroundColor: 'background.default', zIndex: 1000 }}>
-            <Toolbar component="nav">
-                <Container maxWidth="lg">
-                    <Grid container alignItems="center" justifyContent="space-between">
-                        <Grid item>
-                            <Logo sx={{ width: '48px', height: '48px' }} />
-                        </Grid>
-                        <Grid item sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' }, justifyContent: 'flex-end' }}>
-                            <RenderDesktopMenu />
-                            <RenderMobileMenu />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Toolbar>
-        </AppBar>
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children ?? <div />}
+        </Slide>
+    );
+}
+function Header(props: Props) {
+    return (
+        <>
+            <HideOnScroll {...props}>
+                <AppBar role="banner" sx={{ backgroundColor: 'background.default', zIndex: 1000 }}>
+                    <Toolbar component="nav">
+                        <Container maxWidth="lg">
+                            <Grid container alignItems="center" justifyContent="space-between">
+                                <Grid item>
+                                    <Logo sx={{ width: '48px', height: '48px' }} />
+                                </Grid>
+                                <Grid item sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' }, justifyContent: 'flex-end' }}>
+                                    <RenderDesktopMenu />
+                                    <RenderMobileMenu />
+                                </Grid>
+                            </Grid>
+                        </Container>
+                    </Toolbar>
+                </AppBar>
+            </HideOnScroll>
+        </>
     )
 }
 
