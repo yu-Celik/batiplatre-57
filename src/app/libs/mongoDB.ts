1d5bf548d034107f5cd1c1d47888693a860388a3
yu-Celik <celik.yus@outlook.com>
2024-08-31 00:03:51 +0200

    test5

--- src/app/libs/mongoDB.ts
+++ src/app/libs/mongoDB.ts
@@ -3,33 +3,32 @@ import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const connectDB = async (): Promise<mongoose.Connection> => {
-    console.log('Connecting to MongoDB...');
+    console.log('Checking MongoDB connection state...');
     const connectionState = mongoose.connection.readyState;
 
     if (connectionState === 1) {
         console.log('Already connected to MongoDB');
-        return mongoose.connection;  // Retourner directement la connexion
-    }
-    if (connectionState === 2) {
+        return mongoose.connection;
+    } else if (connectionState === 2) {
+        console.log('Connection to MongoDB is currently being established...');
+        return new Promise((resolve, reject) => {
+            mongoose.connection.once('open', resolve);
+            mongoose.connection.once('error', reject);
+        });
+    } else {
         console.log('Connecting to MongoDB...');
-        return new Promise(resolve => {
-            mongoose.connection.once('open', () => {
-                resolve(mongoose.connection);
+        console.log(MONGODB_URI);
+        try {
+            await mongoose.connect(MONGODB_URI!, {
+                dbName: 'batiplatre57',
+                bufferCommands: false,
             });
-        });
-    }
-
-    try {
-        console.log('Connecting to MongoDB 2...');
-        await mongoose.connect(MONGODB_URI!, {
-            dbName: 'batiplatre57',
-            bufferCommands: false,
-        });
-        console.log('Connected to MongoDB');
-        return mongoose.connection;  // Retourner directement la connexion
-    } catch (error) {
-        console.error('Failed to connect to MongoDB:', error);
-        throw new Error('Failed to connect to MongoDB');
+            console.log('Connected to MongoDB');
+            return mongoose.connection;
+        } catch (error) {
+            console.error('Failed to connect to MongoDB:', error);
+            throw new Error('Failed to connect to MongoDB');
+        }
     }
 };
 
